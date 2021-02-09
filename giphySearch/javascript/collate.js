bf489d17866f1d261e7ebedf49a34dbbc4c6abdf
PlacidBeetle <Beetleaverage78@users.noreply.github.com>
2021-02-09 15:22:31 +1300

    Final Product of Giphy Search
    Inlcuded are files from JSZIP, FileSaverAPI.
    
    GiphyAPI usage is cdn based.

---
+++ giphySearch/javascript/collate.js
@@ -0,0 +1,100 @@
+var collateList = [];
+var collateTab = document.querySelector('.collate-tab');
+
+
+function zipZeFiles() {
+  if (collateList.length == 0) {
+    alert("No gifs were selected");
+  } else {
+    var zip = new JSZip();
+    var gifFolder = zip.folder("gifs");
+    var index = 0;
+
+    function loadAsArrayBuffer(url, callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.responseType = "arraybuffer";
+      xhr.onerror = function () { /* Error Handling*/ };
+      xhr.onload = function () {
+        if (xhr.status = 200) { callback(xhr.response, url) }
+        else { /* Error Handling*/ }
+      };
+      xhr.send();
+    }
+
+    function load() {
+      if (index < collateList.length) {
+        loadAsArrayBuffer(collateList[index++], function (buffer, url) {
+          var filename = "giphy" + index + ".gif";
+          gifFolder.file(filename, buffer);
+          load();
+        })
+      } else {
+        zip.generateAsync({ type: "blob" }).then(function (c) {
+          console.log(c);
+          saveAs(c, "collatedGifs.zip");
+        });
+      }
+    };
+
+    function getFilename(url) {
+      return url.substr(url.lastIndexOf("/") + 1);
+    }
+
+    load();
+  }
+}
+
+function addCollate(gif) {
+  var gifBtnIcon = gif.querySelector('.img-overlay');
+  gifBtnIcon.innerHTML = "";
+
+  collateTab.appendChild(gif);
+  collateList.push(gif.querySelector('#imgSrc').src);
+}
+
+function tooLazy() {
+  var collateBtn = document.querySelector('.container-collateBtn');
+  var btnIcon = document.querySelector('.clt');
+
+  collateBtn.style.backgroundColor = "red";
+  btnIcon.classList.remove('fa-file-archive');
+  btnIcon.classList.add('fa-times');
+}
+
+function tooLazyV2() {
+  var collateBtn = document.querySelector('.container-collateBtn');
+  var btnIcon = document.querySelector('.clt');
+
+  collateBtn.style.backgroundColor = "#3b5b6d";
+  btnIcon.classList.remove('fa-times');
+  btnIcon.classList.add('fa-file-archive');
+
+  console.log(collateBtn);
+  console.log(btnIcon);
+}
+
+function toggleCollate(search) {
+  if (collateList.length != 0 && search === false) {
+    var error = document.querySelector('.error-empty');
+    error.style.display = "none";
+  }
+
+  var collateContent = document.querySelector('.collate-tab');
+  var searchContent = document.querySelector('.js-searchContent');
+
+  if ((collateContent.style.display === "" || collateContent.style.display === "none") && search === false) {
+    searchContent.style.display = "none";
+    collateContent.style.display = "block";
+    tooLazy();
+  } else if (search === true) {
+    collateContent.style.display = "none";
+    searchContent.style.display = "block";
+    tooLazyV2();
+  } else {
+    collateContent.style.display = "none";
+    searchContent.style.display = "block";
+    tooLazyV2();
+  }
+
+}
---
+++ giphySearch/javascript/main.js
@@ -0,0 +1,90 @@
+/* Listeners */
+// Button Listener
+document.querySelector(".js-search").addEventListener('click', function () {
+  checkInput();
+});
+// Keyboard Listener
+document.querySelector(".js-userinput").addEventListener('keyup', function (key) {
+  if (key.which === 13) {
+    checkInput();
+  }
+});
+
+/* Grab the Input */
+function checkInput() {
+  var grabInput = document.querySelector(".js-userinput");
+  if (grabInput.value != "") {
+    toggleCollate(true);
+    searchGif(grabInput.value);
+  } else { // If button clicked & search bar empty
+    alert("Search Bar Empty");
+  }
+}
+
+class showGif {
+  showGif() { }
+
+  /* Display Elements (GIFS) */
+  pushToDOM(input) {
+    var container = document.querySelector(".js-searchContent");
+    container.innerHTML = ""; // Clear the Gifs if any
+
+    var response = JSON.parse(input);
+    var imgURLs = response.data;
+
+    imgURLs.forEach(async function (img) {
+      var src = img.images.fixed_height.url;
+
+      var imgContainer = document.createElement('div');
+      imgContainer.classList.add('container-img');
+
+      var img = document.createElement('img');
+      img.id = "imgSrc";
+      img.setAttribute('src', src)
+
+      imgContainer.appendChild(img);
+      container.appendChild(imgContainer);
+
+      var overlay = document.createElement('div');
+      var overlayBtn = document.createElement('button');
+      overlayBtn.addEventListener('click', function () {
+        addCollate(imgContainer);
+      });
+
+      var iconSpan = document.createElement('span');
+      var icon = document.createElement('i');
+
+      overlay.classList.add('img-overlay');
+      overlayBtn.classList.add('icon');
+      icon.classList.add('fas', 'fa-plus-circle');
+
+      iconSpan.appendChild(icon);
+      overlayBtn.appendChild(iconSpan);
+      overlay.appendChild(overlayBtn);
+      imgContainer.append(overlay);
+
+      console.log(imgContainer);
+    });
+  };
+}
+
+/* Communicate with Giphy API */
+var showGifClass = new showGif();
+function searchGif(searchQuery) {
+  var api_key = "Js40wDasv9TaNwe4j6g6S2yohmdBLonn";
+  var limit = 20;
+
+  var url = "http://api.giphy.com/v1/gifs/search?api_key=" + api_key + "&q=" + searchQuery + "&limit=" + limit;
+
+  var giphyCall = new XMLHttpRequest();
+  giphyCall.open('GET', url);
+  giphyCall.send();
+
+  giphyCall.addEventListener('load', function (e) {
+    var gifData = e.target.response;
+    showGifClass.pushToDOM(gifData);
+  });
+}
+
+
+
